50132924184c4d6b285311d72050ae96f9e74667
Fredrik <fredrik.makila@gmail.com>
2013-09-25 18:14:59 +0900

    Added changes to the constructor so it creates its own tokens and toString

--- WeaklySupervisedMethodForComparatorMining/src/ax/makila/comparableentititymining/sequentialpatterns/patterns/GeneralizedSequence.java
+++ WeaklySupervisedMethodForComparatorMining/src/ax/makila/comparableentititymining/sequentialpatterns/patterns/GeneralizedSequence.java
@@ -1,20 +1,23 @@
 package ax.makila.comparableentititymining.sequentialpatterns.patterns;
 
+import ax.makila.comparableentititymining.postagger.StanfordPosTagger;
 import ax.makila.comparableentititymining.sequentialpatterns.Sequence;
 
 public class GeneralizedSequence extends Sequence implements Pattern {
 	public GeneralizedSequence(String seq) {
 		super(seq);
-	}
-
-	@Override
-	public boolean isLexical() {
-		return false;
+		tokens = StanfordPosTagger.tokenizeStringMergeComp(text);
+		toString = StanfordPosTagger.tokensToString(tokens);
 	}
 
 	@Override
 	public boolean isGeneralized() {
 		return true;
+	}
+
+	@Override
+	public boolean isLexical() {
+		return false;
 	}
 
 	@Override
--- WeaklySupervisedMethodForComparatorMining/src/ax/makila/comparableentititymining/sequentialpatterns/patterns/SpecializedSequence.java
+++ WeaklySupervisedMethodForComparatorMining/src/ax/makila/comparableentititymining/sequentialpatterns/patterns/SpecializedSequence.java
@@ -1,19 +1,23 @@
 package ax.makila.comparableentititymining.sequentialpatterns.patterns;
 
+import ax.makila.comparableentititymining.postagger.StanfordPosTagger;
 import ax.makila.comparableentititymining.sequentialpatterns.Sequence;
 
 public class SpecializedSequence extends Sequence implements Pattern {
 	public SpecializedSequence(String seq) {
 		super(seq);
-	}
-
-	@Override
-	public boolean isLexical() {
-		return false;
+		tokens = StanfordPosTagger.tokenizeStringMergeComp(text);
+		toString = StanfordPosTagger.tokensToString(tokens);
+		System.out.println(toString);
 	}
 
 	@Override
 	public boolean isGeneralized() {
+		return false;
+	}
+
+	@Override
+	public boolean isLexical() {
 		return false;
 	}
 
